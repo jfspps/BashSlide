b170ed6ec07b9caa1aab5fe4590806bbf823b9ac
jfspps <jfsapps@gmail.com>
2020-07-01 10:10:26 +0100

    added Shell script to allow TXT file load

---
+++ bin/Release/bashslide.sh
@@ -0,0 +1,3 @@
+# This script runs BashSlide from any directory with its text files
+# It runs a subshell without changing the current working directory
+(cd ~/Dev/BashSlide/bin/Release/ && ./BashSlide)
