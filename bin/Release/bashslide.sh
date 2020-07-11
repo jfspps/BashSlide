bd72a721e65248c30ca966af3a6bd98b71f9d6cc
jfspps <jfsapps@gmail.com>
2020-07-11 19:02:29 +0100

    updated the sh instructions

--- bin/Release/bashslide.sh
+++ bin/Release/bashslide.sh
@@ -1,6 +1,10 @@
 # This script runs BashSlide from any directory with its text files
-# It runs a subshell by assuming that you are in the BashSlide directory without changing the current working directory
-# There are two parts, the path and then the executable. Change the path to locate the BashSlide directory (note
-# that BashSlide does not actually appear in the path) and save this script. ~ is the /home directory
+# It runs a subshell by temporarily hopping to the directory given by the first parameter, running the command given by the second parameter and then 
+# coming back to the directory where the console is currently set to
 
-(cd ~/Dev/BashSlide/bin/Release/ && ./BashSlide)
+# Initially, I extracted BashSlide to a (hidden) directory called .bashSlide in my HOME directory
+# All text files, the BashSlide executable and this script are located in .bashSlide
+
+# If you extracted to a different path, change the first parameter to match the path to the BashSlide directory (not the executable itself) but leave 
+# the second parameter as is
+(cd ~/.bashSlide/ && ./BashSlide)
